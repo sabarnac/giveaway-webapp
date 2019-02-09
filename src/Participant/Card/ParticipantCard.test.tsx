54d4bdec936fdd5bf335ad997d640c883cff1d68
Sabarna Chakravarty <sabarnac@gmail.com>
2019-02-10 01:56:10 +0530

    Add Test Dependencies, Cases

---
+++ src/Participant/Card/ParticipantCard.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ParticipantCard from "./ParticipantCard";
+import Participant, { Avatar } from "../../Store/Participant";
+import renderer from "react-test-renderer";
+
+describe("ParticipantCard Component.", () => {
+  it("Matches snapshot.", () => {
+    const avatar = new Avatar("foobar", "barfoo");
+    const participant = new Participant("fozbaz", avatar);
+    const component = renderer.create(
+      <ParticipantCard participant={participant} />
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
---
+++ src/Participant/Entry/ParticipantEntry.test.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ParticipantEntry from "./ParticipantEntry";
+import Participant, { Avatar } from "../../Store/Participant";
+import renderer from "react-test-renderer";
+
+describe("ParticipantEntry Component.", () => {
+  it("Matches snapshot.", () => {
+    const avatar = new Avatar("foobar", "barfoo");
+    const participant = new Participant("fozbaz", avatar);
+    const component = renderer.create(
+      <ParticipantEntry participant={participant} />
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
