fd3df9a3e103029adbcdb4d040948ffe64401453
Sabarna Chakravarty <sabarnac@gmail.com>
2019-02-10 18:56:32 +0530

    Add Tests

---
+++ src/tournament/TournamentView.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TournamentView from "./TournamentView";
+import Tournament from "../store/Tournament";
+import { create, ReactTestRenderer } from "react-test-renderer";
+import { createDummyTournament } from "../util/test";
+
+describe("TournamentView Component.", () => {
+  it("Matches snapshot.", () => {
+    const tournament: Tournament = createDummyTournament();
+    const component: ReactTestRenderer = create(
+      <TournamentView
+        tournament={tournament}
+        roundId={tournament.firstRound.id}
+        matchId={tournament.firstRound.firstMatch.id}
+      />
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
--- src/util/test/index.ts
+++ src/util/test/index.ts
@@ -3,6 +3,8 @@ import Config from "../../store/config/Config";
 import Participant, {
   Avatar
 } from "../../store/round/match/participant/Participant";
+import Round from "../../store/round/Round";
+import Tournament from "../../store/Tournament";
 
 export const createDummyParticipant = function(id: number = 1): Participant {
   return new Participant(
@@ -16,4 +18,19 @@ export const createDummyMatch = (): Match => {
     createDummyParticipant(1),
     createDummyParticipant(2)
   ]);
+};
+
+export const createDummyRound = (): Round => {
+  return new Round(Config.instance, [
+    createDummyParticipant(1),
+    createDummyParticipant(2),
+    createDummyParticipant(3),
+    createDummyParticipant(4),
+    createDummyParticipant(5),
+    createDummyParticipant(6)
+  ]);
+};
+
+export const createDummyTournament = (): Tournament => {
+  return new Tournament(Config.instance);
 };
