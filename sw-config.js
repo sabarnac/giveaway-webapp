e0756943c9a636c97aa1f83a6675dfd2ec70e4ce
Sabarna Chakravarty <sabarnac@gmail.com>
2019-02-21 02:52:00 +0530

    Fix SW Bugs

---
+++ sw-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    runtimeCaching: [{
+        urlPattern: "/giveaway-webapp/docs",
+        handler: "networkFirst"
+    }, {
+        urlPattern: "/giveaway-webapp/docs/",
+        handler: "networkFirst"
+    }],
+    replacePrefix: "/giveaway-webapp"
+};
