afd56f5606cde90de14e978d5bf00a3ddf686261
Sabarna Chakravarty <sabarnac@gmail.com>
2019-02-28 18:38:50 +0530

    Enable SW Waiting Skip

--- sw-config.js
+++ sw-config.js
@@ -1,10 +1,14 @@
 module.exports = {
-    runtimeCaching: [{
-        urlPattern: "/giveaway-webapp/docs",
-        handler: "networkFirst"
-    }, {
-        urlPattern: "/giveaway-webapp/docs/",
-        handler: "networkFirst"
-    }],
-    replacePrefix: "/giveaway-webapp"
+  runtimeCaching: [
+    {
+      urlPattern: "/giveaway-webapp/docs",
+      handler: "networkFirst",
+    },
+    {
+      urlPattern: "/giveaway-webapp/docs/",
+      handler: "networkFirst",
+    },
+  ],
+  replacePrefix: "/giveaway-webapp",
+  skipWaiting: true,
 };
