7f73ddd815a145bb44c86203689d2805f6a6494e
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-10 21:08:34 +0530

    Fix WinnerOverlay Test

--- src/tournament/winner-overlay/WinnerOverlay.test.tsx
+++ src/tournament/winner-overlay/WinnerOverlay.test.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import WinnerOverlay from "./WinnerOverlay";
 import { create, ReactTestRenderer } from "react-test-renderer";
-import { createDummyConfig, createDummyParticipant } from "../../util/test";
-import Config from "../../store/config/Config";
+import { createDummyParticipant } from "../../util/test";
 
 describe("WinnerOverlay Component.", () => {
   it("Matches snapshot.", () => {
-    const config: Config = createDummyConfig();
     const component: ReactTestRenderer = create(
-      <WinnerOverlay config={config} winner={createDummyParticipant(1)} />
+      <WinnerOverlay show={true} winner={createDummyParticipant(1)} />,
     );
 
     expect(component.toJSON()).toMatchSnapshot();
