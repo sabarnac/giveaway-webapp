bd55701c957b96ebfaf9422684eb2985857397c2
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-10 21:22:10 +0530

    Add Round View Partials Tests

---
+++ src/tournament/round/_partial/RoundDetails.test.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import RoundDetails from "./RoundDetails";
+import Round from "../../../store/round/Round";
+import { create, ReactTestRenderer } from "react-test-renderer";
+import { createDummyRound } from "../../../util/test";
+
+describe("RoundDetails Component.", () => {
+  it("Roundes snapshot.", () => {
+    const round: Round = createDummyRound();
+    const component: ReactTestRenderer = create(
+      <RoundDetails
+        className="foobar"
+        show={true}
+        round={round}
+        matchId={round.firstMatch.id}
+        onRoundComplete={() => {}}
+      />,
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
