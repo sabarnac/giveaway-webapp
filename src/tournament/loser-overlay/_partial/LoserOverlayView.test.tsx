ff777accae4ef0f02748cc1b390a465137a3f3ae
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-10 21:13:18 +0530

    Add Loser Overlay Partials Tests

---
+++ src/tournament/loser-overlay/_partial/LoserOverlayView.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import LoserOverlayView from "./LoserOverlayView";
+import { create, ReactTestRenderer } from "react-test-renderer";
+import { createDummyParticipant } from "../../../util/test";
+import Participant from "../../../store/round/match/participant/Participant";
+
+describe("LoserOverlayView Component.", () => {
+  it("Matches snapshot.", () => {
+    const losers: Participant[] = [
+      createDummyParticipant(1),
+      createDummyParticipant(2),
+      createDummyParticipant(3),
+      createDummyParticipant(4),
+    ];
+    const component: ReactTestRenderer = create(
+      <LoserOverlayView
+        className="foobar"
+        losers={losers}
+        onViewComplete={() => {}}
+      />,
+    );
+
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
