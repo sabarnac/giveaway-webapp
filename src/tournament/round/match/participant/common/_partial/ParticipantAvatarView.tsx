6e464b2e5209fd051c32253ec1c704d2aab45629
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-10 21:34:23 +0530

    Fix Participant Partials Comments

--- src/tournament/round/match/participant/common/_partial/ParticipantAvatarView.tsx
+++ src/tournament/round/match/participant/common/_partial/ParticipantAvatarView.tsx
@@ -4,19 +4,17 @@ import classNames from "classnames";
 import Participant from "../../../../../../store/round/match/participant/Participant";
 
 /**
- * Properties of the participant card React component.
+ * Properties of the participant avatar React component.
  */
 interface ParticipantAvatarViewProps {
   /** CSS class */
   className: string;
   /** The participant details. */
   participant: Participant;
-  /** Whether to invert the color of the participant view or not. */
-  invert?: boolean;
 }
 
 /**
- * Function that returns a React component for the participant view.
+ * Function that returns a React component for the participant avatar.
  */
 export default observer(
   (props: ParticipantAvatarViewProps): JSX.Element => (
--- src/tournament/round/match/participant/common/_partial/ParticipantNameView.tsx
+++ src/tournament/round/match/participant/common/_partial/ParticipantNameView.tsx
@@ -4,19 +4,17 @@ import classNames from "classnames";
 import Participant from "../../../../../../store/round/match/participant/Participant";
 
 /**
- * Properties of the participant card React component.
+ * Properties of the participant name React component.
  */
 interface ParticipantNameViewProps {
   /** CSS class */
   className: string;
   /** The participant details. */
   participant: Participant;
-  /** Whether to invert the color of the participant view or not. */
-  invert?: boolean;
 }
 
 /**
- * Function that returns a React component for the participant view.
+ * Function that returns a React component for the participant name.
  */
 export default observer(
   (props: ParticipantNameViewProps): JSX.Element => (
