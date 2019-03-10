36d59b4d8f0bc81a725582c9644991b57a2a426f
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-11 01:17:52 +0530

    Update Tests

--- src/util/test/index.ts
+++ src/util/test/index.ts
@@ -5,10 +5,7 @@ import Round from "../../store/round/Round";
 import Tournament from "../../store/Tournament";
 
 export const createDummyParticipant = function(id: number = 1): Participant {
-  return new Participant(`fozbaz-${id}`, {
-    url: `foobar-${id}`,
-    altText: `barfoo-${id}`,
-  });
+  return new Participant(`fozbaz-${id}`);
 };
 
 export const createDummyConfig = (): Config => {
