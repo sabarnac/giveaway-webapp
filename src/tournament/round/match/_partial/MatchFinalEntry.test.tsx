fd7073130be04126a596a069f3088029f43d4c9e
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-10 22:43:47 +0530

    Add Match View Partials Tests

---
+++ src/tournament/round/match/_partial/MatchFinalEntry.test.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import MatchFinalEntry from "./MatchFinalEntry";
+import Match from "../../../../store/round/match/Match";
+import { create, ReactTestRenderer } from "react-test-renderer";
+import { createDummyMatch } from "../../../../util/test";
+
+describe("MatchFinalEntry Component.", () => {
+  describe("Current And Actual Match.", () => {
+    it("Matches snapshot.", () => {
+      const match: Match = createDummyMatch();
+      const component: ReactTestRenderer = create(
+        <MatchFinalEntry
+          match={match}
+          className="foobar"
+          isCurrentMatch={true}
+          isActualMatch={true}
+        />,
+      );
+
+      expect(component.toJSON()).toMatchSnapshot();
+    });
+  });
+  describe("Not Current But Actual Match.", () => {
+    it("Matches snapshot.", () => {
+      const match: Match = createDummyMatch();
+      const component: ReactTestRenderer = create(
+        <MatchFinalEntry
+          match={match}
+          className="foobar"
+          isCurrentMatch={false}
+          isActualMatch={true}
+        />,
+      );
+
+      expect(component.toJSON()).toMatchSnapshot();
+    });
+  });
+  describe("Current But Not Actual Match.", () => {
+    it("Matches snapshot.", () => {
+      const match: Match = createDummyMatch();
+      const component: ReactTestRenderer = create(
+        <MatchFinalEntry
+          match={match}
+          className="foobar"
+          isCurrentMatch={true}
+          isActualMatch={false}
+        />,
+      );
+
+      expect(component.toJSON()).toMatchSnapshot();
+    });
+  });
+  describe("Not Current Nor Actual Match.", () => {
+    it("Matches snapshot.", () => {
+      const match: Match = createDummyMatch();
+      const component: ReactTestRenderer = create(
+        <MatchFinalEntry
+          match={match}
+          className="foobar"
+          isCurrentMatch={false}
+          isActualMatch={false}
+        />,
+      );
+
+      expect(component.toJSON()).toMatchSnapshot();
+    });
+  });
+});
