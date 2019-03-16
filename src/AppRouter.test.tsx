68958219cc97602378b7d5770245f92159e5ca98
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-16 14:38:31 +0530

    Split AppRouter React Component And Wrappers

--- src/AppRouter.test.tsx
+++ src/AppRouter.test.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import AppRouter from "./AppRouter";
+import AppRouter from "./AppRouter.react";
 import Tournament from "./store/Tournament";
 import { create, ReactTestRenderer } from "react-test-renderer";
-import { createDummyTournament } from "./util/test";
+import { createDummyTournament, createDummyRouterProps } from "./util/test";
 
 describe("AppRouter Component.", (): void => {
   it("Matches snapshot.", (): void => {
     const tournament: Tournament = createDummyTournament();
     const component: ReactTestRenderer = create(
-      <AppRouter tournament={tournament} />,
+      <AppRouter tournament={tournament} {...createDummyRouterProps()} />,
     );
 
     expect(component.toJSON()).toMatchSnapshot();
