501421ffb7b1cf9bbe10d9e89e7defcc5e4ba6be
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-16 12:03:27 +0530

    Split MatchOverlayParticipants React Component And Wrappers

--- src/tournament/round/match/overlay/_partial/MatchOverlayParticipants.test.tsx
+++ src/tournament/round/match/overlay/_partial/MatchOverlayParticipants.test.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import MatchOverlayParticipants from "./MatchOverlayParticipants";
+import MatchOverlayParticipants from "./MatchOverlayParticipants.react";
 import { create, ReactTestRenderer } from "react-test-renderer";
 import Match from "../../../../../store/round/match/Match";
-import { createDummyMatch } from "../../../../../util/test";
+import {
+  createDummyMatch,
+  createDummyTranslationProps,
+} from "../../../../../util/test";
 
 describe("MatchOverlayParticipants Component.", () => {
   it("Matches snapshot.", () => {
     const match: Match = createDummyMatch();
     const component: ReactTestRenderer = create(
-      <MatchOverlayParticipants className="foobar" currentMatch={match} />,
+      <MatchOverlayParticipants
+        className="foobar"
+        currentMatch={match}
+        {...createDummyTranslationProps()}
+      />,
     );
 
     expect(component.toJSON()).toMatchSnapshot();
--- src/tournament/round/match/overlay/_partial/MatchOverlayParticipants.tsx
+++ src/tournament/round/match/overlay/_partial/MatchOverlayParticipants.tsx
@@ -1,50 +1,8 @@
-import React from "react";
-import { observer } from "mobx-react";
 import "./MatchOverlayParticipants.scss";
-import classNames from "classnames";
-import Match from "../../../../../store/round/match/Match";
-import Participant from "../../../../../store/round/match/participant/Participant";
-import ParticipantCard from "../../participant/card/ParticipantCard";
-import { Trans, WithTranslation, withTranslation } from "react-i18next";
-
-/**
- * Properties of the match overlay participants React component.
- */
-interface MatchOverlayParticipantsProps extends WithTranslation {
-  /** CSS class */
-  className: string;
-  /** The current match details. */
-  currentMatch: Match;
-}
+import { withTranslation } from "react-i18next";
+import MatchOverlayParticipants from "./MatchOverlayParticipants.react";
 
 /**
  * React component for the match overlay participants.
  */
-export default withTranslation()(
-  observer(
-    (props: MatchOverlayParticipantsProps): JSX.Element => (
-      <div className={classNames(`${props.className}__list`)}>
-        {props.currentMatch.participants
-          .flatMap(
-            (participant: Participant): [JSX.Element, JSX.Element] => [
-              <div key={participant.id}>
-                <ParticipantCard invert={true} participant={participant} />
-              </div>,
-              <h3
-                className={classNames("versus-text")}
-                key={`${participant.id} versus`}
-              >
-                <Trans
-                  i18nKey="matchOverlay.versus"
-                  count={props.currentMatch.participants.length}
-                >
-                  VS
-                </Trans>
-              </h3>,
-            ],
-          )
-          .slice(0, -1)}
-      </div>
-    ),
-  ),
-);
+export default withTranslation()(MatchOverlayParticipants);
