e37dac81fd3ca0326fb45fbc764806d13be036be
Sabarna Chakravarty <sabarnac@gmail.com>
2019-03-16 13:48:05 +0530

    Remove Unnecessary Observer HOC

--- src/tournament/round/match/_partial/MatchParticipantList.tsx
+++ src/tournament/round/match/_partial/MatchParticipantList.tsx
@@ -1,7 +1,6 @@
-import { observer } from "mobx-react";
 import MatchParticipantList from "./MatchParticipantList.react";
 
 /**
  * React component for the match participant list.
  */
-export default observer(MatchParticipantList);
+export default MatchParticipantList;
