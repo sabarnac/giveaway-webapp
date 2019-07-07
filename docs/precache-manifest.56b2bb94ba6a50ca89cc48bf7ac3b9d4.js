4ed76f16ca2b64216805184061aefa5f4406ef83
Sabarna Chakravarty <sabarnac@gmail.com>
2019-07-08 00:44:56 +0530

    Demo View

---
+++ docs/precache-manifest.56b2bb94ba6a50ca89cc48bf7ac3b9d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87cc44dd5650e291ab9",
+    "url": "build/2.a87cc44d.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "f38383b8779a9f3afd3c",
+    "url": "build/main.f38383b8.js"
+  },
+  {
+    "revision": "f83e478f1621043004ed2e41bb24848c",
+    "url": "index.html"
+  },
+  {
+    "revision": "f38383b8779a9f3afd3c",
+    "url": "static/css/main.28e17ad5.chunk.css"
+  }
+]);
---
+++ precache-manifest.2b28da9952296fc04e94ade14e62bb69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b97a5d5b20081a038c86313d202d7c",
+    "url": "/giveaway-webapp/index.html"
+  },
+  {
+    "revision": "8b13067fca26fa74e2a7",
+    "url": "/giveaway-webapp/static/css/2.9d1f3529.chunk.css"
+  },
+  {
+    "revision": "143e33b7aa0f9e001467",
+    "url": "/giveaway-webapp/static/css/main.1f248b58.chunk.css"
+  },
+  {
+    "revision": "8b13067fca26fa74e2a7",
+    "url": "/giveaway-webapp/static/js/2.8ff7d91a.chunk.js"
+  },
+  {
+    "revision": "143e33b7aa0f9e001467",
+    "url": "/giveaway-webapp/static/js/main.68712812.chunk.js"
+  },
+  {
+    "revision": "abbdb4032db2e231fc57",
+    "url": "/giveaway-webapp/static/js/runtime~main.0eed3681.js"
+  }
+]);
