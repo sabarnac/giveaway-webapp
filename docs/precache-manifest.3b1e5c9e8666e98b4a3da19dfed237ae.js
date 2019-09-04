dab31cfefc7e1dce03f58b827e12b666fb369c21
Sabarna Chakravarty <sabarnac@gmail.com>
2019-09-04 20:18:03 +0530

    Updates [ci skip]

--- docs/precache-manifest.3b1e5c9e8666e98b4a3da19dfed237ae.js
+++ docs/precache-manifest.3b1e5c9e8666e98b4a3da19dfed237ae.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ee99ea462cd8ecaedb2a",
-    "url": "build/2.ee99ea46.js"
+    "revision": "1415f77255bc3ec26246",
+    "url": "build/2.1415f772.js"
   },
   {
     "revision": "feb19782ca779508500c",
     "url": "build/bundle.feb19782.js"
   },
   {
-    "revision": "90de38c1cd0e401c0361",
-    "url": "build/main.90de38c1.js"
+    "revision": "8077bbf094bd8a386c1c",
+    "url": "build/main.8077bbf0.js"
   },
   {
-    "revision": "a31ff4d8294bf8d376607fc5ae8466c9",
+    "revision": "69925d556dbab25a7653bd1f3957a85d",
     "url": "index.html"
   },
   {
-    "revision": "90de38c1cd0e401c0361",
+    "revision": "8077bbf094bd8a386c1c",
     "url": "static/css/main.28e17ad5.chunk.css"
   }
 ]);
--- precache-manifest.992258102857ff542bef706bbe5f9980.js
+++ precache-manifest.992258102857ff542bef706bbe5f9980.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3adccc63cf52863e63e92e4141a6b2a1",
+    "revision": "6cb253ae51ad3fb9607645c5da765c42",
     "url": "/giveaway-webapp/index.html"
   },
   {
-    "revision": "38084e4b1c17f6b530d7",
+    "revision": "8b13067fca26fa74e2a7",
     "url": "/giveaway-webapp/static/css/2.9d1f3529.chunk.css"
   },
   {
-    "revision": "2dd246062978204a16f1",
+    "revision": "bbad923875e2bc456ad7",
     "url": "/giveaway-webapp/static/css/main.1f248b58.chunk.css"
   },
   {
-    "revision": "38084e4b1c17f6b530d7",
-    "url": "/giveaway-webapp/static/js/2.9d166945.chunk.js"
+    "revision": "8b13067fca26fa74e2a7",
+    "url": "/giveaway-webapp/static/js/2.8ff7d91a.chunk.js"
   },
   {
-    "revision": "2dd246062978204a16f1",
-    "url": "/giveaway-webapp/static/js/main.387175c3.chunk.js"
+    "revision": "bbad923875e2bc456ad7",
+    "url": "/giveaway-webapp/static/js/main.c561f3bb.chunk.js"
   },
   {
     "revision": "abbdb4032db2e231fc57",
