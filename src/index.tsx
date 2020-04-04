167c33970f18ac373a9ce6eb63bd3aa927b00a5b
Sabarna Chakravarty <sabarnac@gmail.com>
2020-04-04 13:56:13 +0530

    Remove Service Worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.scss";
-import * as serviceWorker from "./serviceWorker";
-import Tournament from "./store/Tournament";
-import Config from "./store/config/Config";
-import { HashRouter } from "react-router-dom";
-import AppRouter from "./AppRouter";
-import { Provider } from "mobx-react";
-import "skeleton-css/css/skeleton.css";
-import "./i18n";
-import ServiceWorkerAlertsConfig from "./store/config/ServiceWorkerAlertsConfig";
+import "./i18n"
+import "./index.scss"
+import "skeleton-css/css/skeleton.css"
+
+import { Provider } from "mobx-react"
+import React from "react"
+import ReactDOM from "react-dom"
+import { HashRouter } from "react-router-dom"
+
+import AppRouter from "./AppRouter"
+import * as serviceWorker from "./serviceWorker"
+import Config from "./store/config/Config"
+import ServiceWorkerAlertsConfig from "./store/config/ServiceWorkerAlertsConfig"
+import Tournament from "./store/Tournament"
 
 const config: Config = Config.getInstance();
 const serviceWorkerAlertsConfig: ServiceWorkerAlertsConfig = ServiceWorkerAlertsConfig.getInstance();
@@ -17,10 +19,11 @@ const serviceWorkerAlertsConfig: ServiceWorkerAlertsConfig = ServiceWorkerAlerts
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register({
-  onSuccess: () => serviceWorkerAlertsConfig.setIsAdded(true),
-  onUpdate: () => serviceWorkerAlertsConfig.setIsUpdated(true),
-});
+// serviceWorker.register({
+//   onSuccess: () => serviceWorkerAlertsConfig.setIsAdded(true),
+//   onUpdate: () => serviceWorkerAlertsConfig.setIsUpdated(true),
+// });
+serviceWorker.unregister();
 
 // Create the tournament store
 const store: Tournament = new Tournament(config);
