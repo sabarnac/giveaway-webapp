1d232a86f32760a23ef7e734189f58addd5974fc
Sabarna Chakravarty <sabarnac@gmail.com>
2020-09-01 13:01:22 +0530

    Register Service Worker

--- src/index.tsx
+++ src/index.tsx
@@ -19,11 +19,11 @@ const serviceWorkerAlertsConfig: ServiceWorkerAlertsConfig = ServiceWorkerAlerts
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.register({
-//   onSuccess: () => serviceWorkerAlertsConfig.setIsAdded(true),
-//   onUpdate: () => serviceWorkerAlertsConfig.setIsUpdated(true),
-// });
-serviceWorker.unregister();
+serviceWorker.register({
+  onSuccess: () => serviceWorkerAlertsConfig.setIsAdded(true),
+  onUpdate: () => serviceWorkerAlertsConfig.setIsUpdated(true),
+});
+// serviceWorker.unregister();
 
 // Create the tournament store
 const store: Tournament = new Tournament(config);
