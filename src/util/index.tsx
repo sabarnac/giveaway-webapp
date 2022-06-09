c8c9d1d71099149daddb52a69d9f129c6661a910
Sabarna Chakravarty <sabarnac@gmail.com>
2022-06-09 15:51:04 +0530

    Update Avatar Generation API
    Switch API used for avatar generation

--- src/util/index.tsx
+++ src/util/index.tsx
@@ -14,7 +14,7 @@ export const isDevEnvironment: boolean = process.env.NODE_ENV === "development";
  * @return {string} The avatar URL.
  */
 export const createAvatarImage = (identifier: string): string => {
-  return `https://api.adorable.io/avatars/480/${identifier}`;
+  return `https://avatars.dicebear.com/api/adventurer-neutral/${identifier}.svg`;
 };
 
 /**
